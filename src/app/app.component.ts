c7649a62eaf1e9620e5a5443d1512dd68fa83299
aleksey <aleksey.havr@gmail.com>
2020-06-15 19:00:13 +0300

    Cleared unnecessary parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `<app-bookmarks></app-bookmarks>`,
-  styleUrls: ['./app.component.scss']
+  template: `<app-bookmarks></app-bookmarks>`
 })
-export class AppComponent {
-  title = 'bookmarks';
-}
+export class AppComponent {}
